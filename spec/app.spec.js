9bdfccbf9c1cfe6e6e2adfe758fcefa3fd439738
Jun Kai Lim <junkaiii@gmail.com>
2016-10-19 20:46:34 +0800

    test cases somewhat complete

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -5,15 +5,15 @@ var expect = require('chai').expect();
 
 
 function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 var propType = ['animal', 'car', 'name'];
 var property = ['cat', 'Vios', 'Kai'];
 var obj = {};
-var utc_number = randomInt(1476670462, 2147483647).toString();
-var index_number = randomInt(0, property.length-1).toString();
-var store_utc;
+var index_number = randomInt(0, property.length - 1).toString();
+var store_utc,
+  store_utc2;
 
 obj[propType[index_number]] = property[index_number];
 
@@ -23,7 +23,7 @@ describe("POST to /object endpoint", function() {
       .post('/object')
       .send(obj)
       .expect(200)
-      .end(function(err,res){
+      .end(function(err, res) {
         console.log(res.body);
         console.log(propType[index_number]);
         res.body.should.have.property(propType[index_number]);
@@ -33,48 +33,42 @@ describe("POST to /object endpoint", function() {
   });
 });
 
-describe("GET /object endpoint", function() {
+describe("GET /object", function() {
   it("returns object saved or cannot be found", function(done) {
     supertest(app)
       .post('/object')
       .send(obj)
       .expect(200)
-      .end(function(){
+      .end(function() {
         supertest(app)
           .get('/object/' + obj[propType[index_number]])
           .expect(200)
-          .end(function(err, res){
+          .end(function(err, res) {
             res.body.should.have.property(propType[index_number]);
-            expect(res.body.utc > store_utc);
+            expect(res.body.utc > store_utc).to.be.ok;
+            //latest utc number generated from second post
+            store_utc2 = res.body.utc;
+            //test exact timestamp
+            supertest(app)
+              .get('/object/' + obj[propType[index_number]] + '?timestamp=' + store_utc2)
+              .expect(200)
+              .end(function(err, res) {
+                //responds with the same object because timestamp can be found
+                expect(store_utc2 == res.body.utc).to.be.ok;
+                done();
+              });
+            //test exact timestamp minus 1
+            supertest(app)
+              .get('/object/' + obj[propType[index_number]] + '?timestamp=' + (store_utc2 - 1))
+              .expect(200)
+              .end(function(err, res) {
+                //responds with the object created with the first post - timestamp cannot be found and therefore show the previous one
+                expect(store_utc == res.body.utc).to.be.ok;
+                done();
+              });
+            done();
           });
+        done();
       });
   });
 });
-
-
-// var checkProperty = function(res) {
-//   res.body.name.should.equal("object", name_to_test);
-// };
-//
-// describe('GET JSON data from params', function() {
-//   it("returns the correct data", function(done) {
-//     supertest(app)
-//       .get('/object/' + name_to_test)
-//       .expect('Content-Type', /json/)
-//       .expect(200)
-//       .expect(checkProperty)
-//       .end(done);
-//   });
-// });
-
-//
-//
-//
-// describe('GET JSON data from query', function() {
-//   it("returns the correct data", function(done) {
-//     supertest(app)
-//       .get('/object/' + name_to_test + '?timestamp=' + utc_number)
-//       .expect('Content-Type', /json/)
-//       .expect(200, res.body.should.have.property("object", name_to_test), done);
-//   });
-// });
